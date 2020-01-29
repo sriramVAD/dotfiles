eebaf5cb214fa6c916ee34fe7a01553607017e79
sriram <sriram.vadlamani@epita.fr>
2020-01-29 13:26:12 +0100

    i3lock.sh added.

---
+++ i3/i3lock.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+scrot /tmp/screen_locked.png
+convert /tmp/screen_locked.png -blur 8x8 /tmp/screen_locked2.png
+i3lock -i /tmp/screen_locked2.png
+
+rm /tmp/screen_locked2.png
+rm /tmp/screen_locked.png
