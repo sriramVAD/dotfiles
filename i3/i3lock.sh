9498bc821fd6eb53b67932757b844425f425c958
Sriram Vadlamani <sriram.vadlamani@epita.fr>
2020-02-02 21:18:28 +0100

    minor i3lock changes

--- i3/i3lock.sh
+++ i3/i3lock.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 scrot /tmp/screen_locked.png
-convert /tmp/screen_locked.png -blur 8x8 /tmp/screen_locked2.png
+convert /tmp/screen_locked.png -blur 20x20 /tmp/screen_locked2.png
 i3lock -i /tmp/screen_locked2.png
 
 rm /tmp/screen_locked2.png
